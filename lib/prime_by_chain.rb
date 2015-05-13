28925f3d0425053f8bc724e5d15adc0aceba049c
Ray Walters <ray@namely.com>
2015-05-12 21:51:04 -0400

    Move the last logic into Processors

--- lib/prime_by_chain.rb
+++ lib/prime_by_chain.rb
@@ -4,13 +4,6 @@ class PrimeByChain
   def is_prime?(input)
     return false if Processors::LessThanTwo.new.not_prime?(input)
 
-    sqrt = Math.sqrt(input)
-    return false if sqrt == sqrt.floor
-
-    (3..(sqrt.floor)).each do |i|
-      return false if (input%i).zero?
-    end
-
     return true
   end
 end
--- lib/processors.rb
+++ lib/processors.rb
@@ -23,7 +23,45 @@ module Processors
     end
 
     def successor
+      Processors::HasSquareRoot.new
+    end
+  end
+
+  class HasSquareRoot
+    def not_prime?(number)
+      sqrt = Math.sqrt(number)
+      if sqrt == sqrt.floor
+        true
+      else
+        successor.not_prime?(number)
+      end
+    end
+
+    def successor
+      Processors::HasIntegerDivisor.new
+    end
+  end
+
+  class HasIntegerDivisor
+    def not_prime?(number)
+      if divisor_found?(number)
+        true
+      else
+        successor.not_prime?(number)
+      end
+    end
+
+    def successor
       Default.new
+    end
+
+    private
+
+    def divisor_found?(number)
+      sqrt = Math.sqrt(number)
+      (3...sqrt).any? do |i|
+        (number%i).zero?
+      end
     end
   end
 
